7a9e93b39db71a73e4107f821f41f41f6fdee86a
BahaaShk <bahaa.shk@gmail.com>
2025-05-17 12:07:40 +0300

    1st fib sequence

--- DSA/fibonaci-sequence.js
+++ DSA/fibonaci-sequence.js
@@ -1,7 +1,11 @@
 function fib(n){
+    let fib = [0,1]
     if(n<=1){
         return n;
     }
-    return fib(n-1) + fib(n-2);
+    for(let i =2; i<n ; i++){
+         fib[i] = fib[i-1]+fib[i-2];
+    }
+    return fib
 }
-console.log(fib(5))
+console.log(fib(7))
