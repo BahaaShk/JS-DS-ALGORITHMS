700a878f2b202e77d4d6fdb5a9aa72867d98f494
BahaaShk <bahaa.shk@gmail.com>
2025-06-13 23:53:38 +0300

    implemented binarySearch algorithm

---
+++ DSA/BinarySearch.js
@@ -0,0 +1,15 @@
+function binarySearch(arr,tar){
+  let leftIndex = 0;
+  let rightIndex = arr.length -1;
+
+while(leftIndex <= rightIndex){
+  let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+}
+if(arr.length === 0) return -1;
+
+if(arr.length > 0){
+
+}
+}
+console.log(binarySearch([10, 20, 30, 40, 50], 30));
+console.log(binarySearch([10, 20, 30, 40, 50], 50));
